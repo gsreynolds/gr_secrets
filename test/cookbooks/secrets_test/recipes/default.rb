b51f6ac25038024da86758ef13e4483f900ee41a
Gavin Reynolds <gavin@chef.io>
2020-01-20 19:13:04 +0000

    Add attribute for vault name
    Signed-off-by: Gavin Reynolds <gavin@chef.io>

--- test/cookbooks/secrets_test/recipes/default.rb
+++ test/cookbooks/secrets_test/recipes/default.rb
@@ -2,5 +2,5 @@ include_recipe 'secrets::default'
 
 # Write the secret to a file:
 file '/etc/config_file' do
-  content lazy { "password = #{akv_get_secret('kitchen-secrets-test', 'test-secret')}" }
+  content lazy { "password = #{akv_get_secret(node['secrets_test']['vault'], 'test-secret')}" }
 end
