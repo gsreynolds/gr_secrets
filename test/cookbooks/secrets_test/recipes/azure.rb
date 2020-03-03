068587d85d4a783397dd79e83ae81821c2814744
Gavin Reynolds <gavin@chef.io>
2020-03-03 22:40:42 +0000

    Uncomment user-assigned managed service identity and SPN examples
    Signed-off-by: Gavin Reynolds <gavin@chef.io>

--- test/cookbooks/secrets_test/recipes/azure.rb
+++ test/cookbooks/secrets_test/recipes/azure.rb
@@ -9,20 +9,20 @@ file '/etc/config_file' do
   content lazy { "password = #{akv_get_secret(vault: vault, secret: secret)}" }
 end
 
-# # Using a user-assigned managed identity that is permitted access to the Key Vault, added to the VM
-# msi_data_bag = data_bag_item('azure_key_vault', 'msi_encrypted')
-# file '/etc/config_file_user_assigned_msi' do
-#   content lazy { "password = #{akv_get_secret(vault: vault, secret: secret, user_assigned_msi: msi_data_bag)}" }
-# end
+# Using a user-assigned managed identity that is permitted access to the Key Vault, added to the VM
+msi_data_bag = data_bag_item('azure_key_vault', 'msi_encrypted')
+file '/etc/config_file_user_assigned_msi' do
+  content lazy { "password = #{akv_get_secret(vault: vault, secret: secret, user_assigned_msi: msi_data_bag)}" }
+end
 
-# # Use a service principal, that has been permitted access to the Key Vault
-# # encrypted_data_bag_secret in client.rb, defaults to /etc/chef/encrypted_data_bag_secret
-# # and C:\chef\encrypted_data_bag_secret. In Test Kitchen it is /tmp/kitchen/encrypted_data_bag_secret
+# Use a service principal, that has been permitted access to the Key Vault
+# encrypted_data_bag_secret in client.rb, defaults to /etc/chef/encrypted_data_bag_secret
+# and C:\chef\encrypted_data_bag_secret.
 
-# # knife data bag create azure_key_vault spn_encrypted --secret-file encrypted_data_bag_secret
-# spn_data_bag = data_bag_item('azure_key_vault', 'spn_encrypted')
+# knife data bag create azure_key_vault spn_encrypted --secret-file encrypted_data_bag_secret
+spn_data_bag = data_bag_item('azure_key_vault', 'spn_encrypted')
 
-# # Write the secret to a file:
-# file '/etc/config_file_spn' do
-#   content lazy { "password = #{akv_get_secret(vault: vault, secret: secret, spn: spn_data_bag)}" }
-# end
+# Write the secret to a file:
+file '/etc/config_file_spn' do
+  content lazy { "password = #{akv_get_secret(vault: vault, secret: secret, spn: spn_data_bag)}" }
+end
