6a7edb10f2f9b0f83f3ddf28d1d10047f1623fd4
Gavin Reynolds <gavin@chef.io>
2020-03-05 12:39:23 +0000

    Pin to azure_keyvault cookbook v0.5.1
    Signed-off-by: Gavin Reynolds <gavin@chef.io>

--- Policyfile.rb
+++ Policyfile.rb
@@ -17,4 +17,4 @@ named_run_list :aws, 'secrets_test::aws'
 # Specify a custom source for a single cookbook:
 cookbook 'secrets_test', path: './test/cookbooks/secrets_test'
 cookbook 'gr_secrets', path: '.'
-cookbook 'azure_keyvault', github: 'gsreynolds/azure_keyvault'
+cookbook 'azure_keyvault', github: 'gsreynolds/azure_keyvault', tag: 'v0.5.1'
